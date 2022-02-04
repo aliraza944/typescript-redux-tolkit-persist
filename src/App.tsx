65dc8d5f3192f24884b10e89526f593a98a4b0ba
aliraza944 <alisabri022@gmail.com>
2022-02-04 20:49:34 +0500

    Added the basic boiler plate styling

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,45 @@ function App() {
   const dispatch = useAppDispatch();
   return (
     <div className="App">
-      <h1>Redux typescript toolkit and redux persist boilerplate</h1>
-      <p>{state}</p>
+      <h1 className="text-3xl font-bold">
+        Redux typescript toolkit and redux persist boilerplate
+      </h1>
+      <p className="text-2xl text-blue-900 font-bold">{state}</p>
       <div>
-        <p>{apiStatus}</p>
+        <p className="mt-4 mb-4 capitalize text-xl">{apiStatus}</p>
+      </div>
+      <div className="flex items-center justify-center">
+        {" "}
+        <div className="w-60 flex items-center justify-between">
+          <button
+            className="border-2 bg-slate-500 p-5 w-20 rounded"
+            onClick={() => dispatch(increment())}
+          >
+            add
+          </button>
+          <button
+            className="border-2 bg-slate-500 p-5 w-20 rounded"
+            onClick={() => dispatch(decrement())}
+          >
+            minus
+          </button>
+        </div>
       </div>
       <div>
-        <button onClick={() => dispatch(increment())}>add</button>
-        <button onClick={() => dispatch(decrement())}>minus</button>
-      </div>
-      <div>
-        <button onClick={() => dispatch(incrementWithPayload(2))}>
+        <button
+          className="border-2 bg-slate-500 p-5 w-40 rounded"
+          onClick={() => dispatch(incrementWithPayload(2))}
+        >
           payload 2
         </button>
       </div>
       <div>
-        <button onClick={() => dispatch(fetchUserById(state))}>Api call</button>
+        <button
+          className="border-2 bg-slate-500 p-5 w-40 rounded"
+          onClick={() => dispatch(fetchUserById(state))}
+        >
+          Api call
+        </button>
       </div>
     </div>
   );
